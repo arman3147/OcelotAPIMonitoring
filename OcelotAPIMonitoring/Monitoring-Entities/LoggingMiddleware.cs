08ad024f199f04745f98add960261c793fdb5ad9
Arman Brki <Arman.brki3147@gmail.com>
2022-01-11 21:40:13 +0330

    Monitoring MiddleWare

--- OcelotAPIMonitoring/Monitoring-Entities/LoggingMiddleware.cs
+++ OcelotAPIMonitoring/Monitoring-Entities/LoggingMiddleware.cs
@@ -1,11 +1,85 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace OcelotAPIMonitoring.Monitoring_Entities
 {
     public class LoggingMiddleware
     {
+        private readonly ILogger<LoggingMiddleware> _logger;
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task<string> FormatRequest(HttpRequest request)
+        {
+            using var reader = new StreamReader(
+                request.Body,
+                encoding: Encoding.UTF8,
+                detectEncodingFromByteOrderMarks: false,
+                leaveOpen: true);
+            var body = await reader.ReadToEndAsync();
+
+            var formattedRequest = $"{request.Scheme} {request.Host}{request.Path} {request.QueryString} {body}";
+
+            request.Body.Position = 0;
+
+            return formattedRequest;
+        }
+
+        public async Task<string> FormatResponse(HttpResponse response)
+        {
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            string text = await new StreamReader(response.Body).ReadToEndAsync();
+
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            return $"{response.StatusCode}: {text}";
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Request.EnableBuffering();
+
+            var builder = new StringBuilder();
+            var request = await FormatRequest(context.Request);
+
+            builder.Append("Request: ").AppendLine(request);
+            builder.AppendLine("Request headers: ");
+            foreach(var header in context.Request.Headers)
+            {
+                builder.Append(header.Key).Append(':').AppendLine(header.Value);
+            }
+
+            var orginalBodyStream = context.Response.Body;
+
+            using var responseBody = new MemoryStream();
+
+            context.Response.Body = responseBody;
+
+            await _next(context);
+
+            var response = await FormatResponse(context.Response);
+            builder.Append("Response: ").AppendLine(response);
+            builder.AppendLine("Response headers:");
+            foreach (var header in context.Response.Headers)
+            {
+                builder.Append(header.Key).Append(':').AppendLine(header.Value);
+            }
+
+            _logger.LogInformation(builder.ToString());
+
+            await responseBody.CopyToAsync(orginalBodyStream);
+        }
     }
 }
