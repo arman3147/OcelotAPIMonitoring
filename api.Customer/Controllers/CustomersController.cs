109f8030ada666da4ca2f1fb3c2fb88e091ff0f5
Arman Brki <Arman.brki3147@gmail.com>
2022-01-12 23:57:02 +0330

    add Customer api

---
+++ api.Customer/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using api.Customer.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Customer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+
+        private readonly ILogger<CustomersController> _logger;
+        private readonly ICustomerRepository _customerRepository;
+        
+
+        public CustomersController(ILogger<CustomersController> logger,ICustomerRepository customerRepository)
+        {
+            _logger = logger;
+            _customerRepository = customerRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Models.Customer>>> Get()
+        {
+            return await _customerRepository.GetAllCustomer();
+        }
+    }
+}
---
+++ api.Customer/Repository/CustomerRepository.cs
@@ -0,0 +1,34 @@
+﻿using api.Customer.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Customer.Repository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private List<Models.Customer> customers = new List<Models.Customer>();
+        public CustomerRepository()
+        {
+            customers.Add(new Models.Customer()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Arman",
+                LastName = "Bahrkazemi",
+                Email = "arman.brki3147@gmail.com"
+            });
+            customers.Add(new Models.Customer()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "majid",
+                LastName = "gholipour :)",
+                Email = "tempEmail@gmail.com"
+            });
+        }
+        public Task<List<Models.Customer>> GetAllCustomer()
+        {
+            return Task.FromResult(customers);
+        }
+    }
+}
