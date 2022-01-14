3042547a1bb905dc113537895f1f18359a74e6cf
Arman Brki <Arman.brki3147@gmail.com>
2022-01-14 16:50:52 +0330

    configure dependency Injection

--- api.Customer/Startup.cs
+++ api.Customer/Startup.cs
@@ -1,3 +1,5 @@
+using api.Customer.Repository;
+using api.Customer.Repository.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,8 +27,8 @@ namespace api.Customer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
